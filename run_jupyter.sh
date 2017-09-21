e4502783ac94972755c37e3024def5bbe0d89c2b
mschermann <mschermann@scu.edu>
2017-09-20 21:53:40 -0700

    Extensions

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 #${VARIABLE} are located in the .env file
+jupyter nbextension enable varInspector/main
+jupyter nbextension enable toc2/main
+jupyter nbextension enable init_cell/main
+jupyter nbextension enable hinterland/hinterland
+jupyter nbextension enable execute_time/ExecuteTime
+jupyter nbextension enable snippets_menu/main
+jupyter nbextension enable exercise2/main
+
 jupyter-notebook --no-browser --notebook-dir=/home/${JUPYTER_USER}/notebooks --ip=0.0.0.0 --port ${JUPYTER_PORT}
